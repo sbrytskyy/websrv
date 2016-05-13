c3fc71b579604257998e19bb4d68eec3868da409
Serhiy Brytskyy <sbrytskyy@gmail.com>
2016-05-13 03:23:53 -0700

    Set test http response. Improved jmeter test.

--- websrv/src/protocol.c
+++ websrv/src/protocol.c
@@ -17,9 +17,8 @@ int process_http(struct SocketContext* pSc)
 	char* index = strstr(pSc->pRequest, "GET");
 	if (index == pSc->pRequest)
 	{
-//		char* response =
-//				"HTTP/1.1 200 OK\r\n<html>\r\n<body>\r\n<h1>Hello, World!</h1>\r\n</body>\r\n</html>";
-		char* response = "Response\n";
+		char* response =
+				"HTTP/1.1 200 OK\r\n\r\n<html>\r\n<body>\r\n<h1>Hello, World!</h1>\r\n</body>\r\n</html>";
 
 		pSc->pResponse = malloc(strlen(response) + sizeof(char));
 		strcpy(pSc->pResponse, response);
